b033cb65147803393c17192bb6847fc844b4792d
DESKTOP-50B12QR\user <Shlomo>
2024-05-27 09:44:57 +0300

    Added User Account Dtos

---
+++ forum-service/src/main/java/telran/java52/accounting/dto/UserRegisterDto.java
@@ -0,0 +1,11 @@
+package telran.java52.accounting.dto;
+
+import lombok.Getter;
+
+public class UserRegisterDto {
+	@Getter
+   String	login;
+   String password;
+    String firstName;
+    String lastName;
+}
